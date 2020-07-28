91f65e9e2cc2e6746af7d5f1a22b42ef5005f13e
twzkraus <twzkraus@gmail.com>
2020-07-28 14:45:09 -0700

    implemented countOccurrence

--- src/recursion.js
+++ src/recursion.js
@@ -351,6 +351,18 @@ var fizzBuzz = function(n) {
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
+  if (array.length === 0) {
+    return 0;
+  }
+
+  if (array[0] === value) {
+    if (array.length === 1) {
+      return 1;
+    }
+    return 1 + countOccurrence(array.slice(1), value);
+  }
+  return countOccurrence(array.slice(1), value);
+
 };
 
 // 21. Write a recursive version of map.
