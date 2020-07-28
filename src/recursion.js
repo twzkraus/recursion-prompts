f5ce024dd5676ae94e1b519bdb5336a0cae06336
twzkraus <twzkraus@gmail.com>
2020-07-28 14:27:09 -0700

    implemented compare strings

--- src/recursion.js
+++ src/recursion.js
@@ -266,7 +266,6 @@ var divide = function(x, y) {
   } else {
     ans = 1 + divide(x - y, y);
   }
-  console.log(ans);
   return ans;
 };
 
@@ -283,6 +282,13 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) {
+    return true;
+  }
+  if (str1[0] !== str2[0] || !(str1[0]) || !(str2[0])) {
+    return false;
+  }
+  return compareStr(str1.slice(1), str2.slice(1));
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter
