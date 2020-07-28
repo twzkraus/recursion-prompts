6d4aa653280ca1aa4a9e3a6570fe3391b35ed21b
twzkraus <twzkraus@gmail.com>
2020-07-28 14:28:49 -0700

    implemented createArray

--- src/recursion.js
+++ src/recursion.js
@@ -294,6 +294,11 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 1) {
+    return [str];
+  }
+
+  return [str[0]].concat(createArray(str.slice(1)));
 };
 
 // 17. Reverse the order of an array
