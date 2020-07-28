3cb98a3f4544e19e9c84a66ec55223a82cb08eb0
twzkraus <twzkraus@gmail.com>
2020-07-28 13:00:00 -0700

    implemented powerOfTwo

--- src/recursion.js
+++ src/recursion.js
@@ -145,6 +145,16 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  // any n should reduce to 1 if a power of 2
+  if (n === 1) {
+    return true;
+  } else if (n < 1) {
+    // if 1 is passed but not hit, it's false
+    return false;
+  }
+
+  // divide by 2 and try again
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
