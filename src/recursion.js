b11f9bc3f98d5ae395014a50f244e470c5c7d156
twzkraus <twzkraus@gmail.com>
2020-07-28 20:01:01 -0700

    implemented compress

--- src/recursion.js
+++ src/recursion.js
@@ -544,6 +544,15 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+  if (list.length === 0) {
+    return [];
+  }
+
+  let i = 0;
+  while (list[i] === list[0]) {
+    i++;
+  }
+  return [list[0]].concat(compress(list.slice(i)));
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
