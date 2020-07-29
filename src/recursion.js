4dff45f30f4f425b9d61a8b5c9db5a5fc9029579
twzkraus <twzkraus@gmail.com>
2020-07-28 19:56:47 -0700

    implemented letterTally

--- src/recursion.js
+++ src/recursion.js
@@ -527,6 +527,15 @@ var flatten = function(array) {
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
 var letterTally = function(str, obj) {
+  if (str.length === 0) {
+    return obj;
+  }
+  if (!obj) {
+    obj = {};
+  }
+  let char = str[0];
+  obj[char] = obj[char] + 1 || 1
+  return letterTally(str.slice(1), obj);
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
